54520b1c48ea96c37b45076a432934464bccb226
Supriyahs1008 <akshayvibhoothiak@gmail.com>
2023-11-30 11:17:20 +0530

    Learned about Jframe

---
+++ GUISwing/src/Main.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();//creating a jframe object
+
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//control what happens when close button is pressed
+        frame.setTitle("Akshay's Jframe");//sets title of frame
+        frame.setSize(400,400);//sets size x and y dimensions
+        frame.setResizable(false);//prevents resizing of frame
+        frame.setVisible(true);//making the jframe visible
+        frame.getContentPane().setBackground(Color.YELLOW);//changing bg color
+        frame.getContentPane().setBackground(new Color(123,50 ,250));//changing bg color with rgb values
+        frame.getContentPane().setBackground(new Color(0xbb22ff));
+
+        ImageIcon icon = new ImageIcon("batlogo.png");//object for icon of frame
+        frame.setIconImage(icon.getImage());//setting icon of frame
+
+
+
+    }
+}
